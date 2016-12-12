55a7db1579414df80b67b3896e11bc5b5709b5c5
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2016-12-13 00:15:05 +0100

    Initial commit

---
+++ recursion.py
@@ -0,0 +1,12 @@
+def mcd(a, b):
+    return mcd(max(a, b) - min(a, b), min(a, b)) if a != b else a
+    # if a == b:
+    #     return a
+    # return mcd(a - b, b) if a > b else mcd(a, b - a)
+
+
+def main():
+    print(mcd(18, 6))
+
+if __name__ == '__main__':
+    main()
---
+++ sieve.py
@@ -0,0 +1,29 @@
+# from functools import partial
+#
+#
+# def multipli(item1, item2):
+#     return item2 % item1 != 0
+
+
+def sieve(n):
+    soe = list(range(2, n))
+    # soe = [x for x in range(2, n)]
+    primes = []
+    while pow(soe[0], 2) <= n:
+        primes.append(soe[0])
+        soe = [x for x in soe[1:] if x % soe[0] != 0]
+
+        # soe = list(filter(partial(multipli, soe[0]), soe[1:]))
+
+        # for x in soe[1:]:
+        #     if x % soe[0] == 0:
+        #         soe.remove(x)
+        # soe = list(filter(lambda x: x % soe[0] != 0, soe[1:]))
+    return primes + soe
+
+
+def main():
+    print(sieve(100))
+
+if __name__ == '__main__':
+    main()
